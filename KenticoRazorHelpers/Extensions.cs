4846c05bd83371d2a4edae74b8add17573e3b34d
vby <vby@nemetos.net>
2015-07-29 13:43:36 +0300

    0.1a

---
+++ KenticoRazorHelpers/Extensions.cs
@@ -0,0 +1,74 @@
+﻿using CMS.Controls;
+using CMS.DocumentEngine;
+using CMS.SiteProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KenticoHelpers
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Get nodes from multiple choice dual list field</summary>
+        /// <remarks>
+        /// Helper for "Multiple Choice Dual List (With Sort)". Download Page:
+        /// devnet.kentico.com/marketplace/inline-controls/dual-list-multi-select-form-(with-sort) </remarks>
+        public static IEnumerable<TreeNode> GetNodesFromMultiList(this TreeNode node, string columnName)
+        {
+            var value = node.GetValue(columnName, "");
+            if (!string.IsNullOrEmpty(value))
+            {
+                var guidVals = value.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+                var guids = new List<TreeNode>();
+                foreach (var v in guidVals)
+                {
+                    Guid g;
+                    var val = Guid.TryParse(v, out g) ? g : Guid.Empty;
+                    var contentNode = GetNode(val, SiteContext.CurrentSite.SiteName);
+                    if (contentNode != null) guids.Add(GetNode(val, SiteContext.CurrentSite.SiteName));
+                };
+                return guids;
+            }
+            return Enumerable.Empty<TreeNode>();
+        }
+
+        /// <summary>
+        /// Get node by Guid</summary>
+        /// <remarks> 
+        /// Return null if nodeGuid is empty or node is not exist </remarks> 
+        public static TreeNode GetNode(Guid nodeGuid, string siteName)
+        {
+            if (nodeGuid != Guid.Empty)
+            {
+                var tp = new TreeProvider();
+
+                return tp.SelectSingleNode(TreePathUtils.GetNodeIdByNodeGUID(nodeGuid, siteName));
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Get url by Guid</summary>
+        public static string GetDocumentLink(Guid nodeGuid, string siteName)
+        {
+            var tp = new TreeProvider();
+            TreeNode tn = tp.SelectSingleNode(TreePathUtils.GetNodeIdByNodeGUID(nodeGuid, siteName));
+            if (tn != null) return tn.RelativeURL;
+            
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Get node by Guid</summary>
+        /// /// <remarks> 
+        /// Get url by guid for current site </remarks>
+        public static string GetDocumentLink(Guid nodeGuid)
+        {
+            return GetDocumentLink(nodeGuid, SiteContext.CurrentSiteName);
+        }
+    }
+}
---
+++ KenticoRazorHelpers/Web.cs
@@ -0,0 +1,105 @@
+﻿using CMS.Controls;
+using CMS.DocumentEngine;
+using CMS.SiteProvider;
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using KenticoHelpers;
+
+namespace KenticoRazorHelpers
+{
+    public static class Web
+    {
+        /// <summary> 
+        /// Get 'img' tag with image url from given node</summary>
+        /// <remarks> 
+        /// Use only for attacment with image in field</remarks> 
+        public static MvcHtmlString GetImage(this TreeNode node, string imgFieldName, object htmlAttributes)
+        {
+            if (node != null && !string.IsNullOrEmpty(imgFieldName))
+            {
+                var th = new TransformationHelper();
+                var outputHtml = new TagBuilder("img");
+                var attributes = HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes);
+                var mediaItemUrl = th.GetFileUrlFromAlias(node.GetValue(imgFieldName), node.NodeAlias);
+                outputHtml.MergeAttribute("src", mediaItemUrl);
+                outputHtml.MergeAttributes(attributes, true);
+
+                return MvcHtmlString.Create(outputHtml.ToString());
+            }
+
+            return MvcHtmlString.Empty;
+        }
+
+        /// <summary> 
+        /// Get 'a' tag with attached file url from given node</summary>
+        /// <remarks> 
+        /// For any attachment types</remarks> 
+        public static MvcHtmlString GetFileLink(this TreeNode node, string fileFieldName, string linkText, object htmlAttributes)
+        {
+            if (node != null && !string.IsNullOrEmpty(fileFieldName))
+            {
+                var th = new TransformationHelper();
+                var outputHtml = new TagBuilder("a")
+                {
+                    InnerHtml =
+                        (!String.IsNullOrEmpty(linkText))
+                            ? HttpUtility.HtmlEncode(linkText)
+                            : String.Empty
+                };
+                var attributes = HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes);
+                var mediaItemUrl = th.GetFileUrlFromAlias(node.GetValue(fileFieldName), node.NodeAlias);
+                outputHtml.MergeAttribute("href", mediaItemUrl);
+                outputHtml.MergeAttributes(attributes, true);
+
+                return MvcHtmlString.Create(outputHtml.ToString());
+            }
+
+            return MvcHtmlString.Empty;
+        }
+
+        /// <summary> 
+        /// Get 'a' tag with url to given node</summary> 
+        public static MvcHtmlString GetDocumentLink(
+            this TreeNode node,
+            string linkText,
+            string siteName,
+            object htmlAttributes)
+        {
+            if (node != null)
+            {
+                var attributes = HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes);
+                var builder = new TagBuilder("a")
+                {
+                    InnerHtml =
+                        (!String.IsNullOrEmpty(linkText))
+                            ? HttpUtility.HtmlEncode(linkText)
+                            : String.Empty
+                };
+                builder.MergeAttribute("href", node.RelativeURL);
+                builder.MergeAttributes(attributes, true);
+
+                return MvcHtmlString.Create(builder.ToString());
+            }
+
+            return MvcHtmlString.Empty;
+        }
+
+        /// <summary> 
+        /// Get 'a' tag with url to node by given guid</summary> 
+        public static MvcHtmlString GetDocumentLink(
+            Guid nodeGuid,
+            string linkText,
+            string siteName,
+            object htmlAttributes)
+        {
+            if (nodeGuid != Guid.Empty)
+            {
+                var tn = Extensions.GetNode(nodeGuid, siteName);
+                return GetDocumentLink(tn, linkText, siteName, htmlAttributes);
+            }
+            return MvcHtmlString.Empty;
+        }
+    }
+}
